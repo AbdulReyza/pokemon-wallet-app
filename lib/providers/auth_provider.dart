135c151f5f5e35d96d9c88ddf41cd5d18394682c
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-16 20:21:51 +0700

    bikin auth provider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,1 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/auth_service.dart';
 
+class AuthProvider extends ChangeNotifier {
+  final AuthService _authService = AuthService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  bool isLoading = false;
+
+  User? _user;
+
+  User? get user => _user;
+
+  AuthProvider() {
+    _user = _firebaseAuth.currentUser;
+  }
+
+  Future<void> login({required String email, required String password}) async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      await _authService.login(email: email, password: password);
+
+      _user = _firebaseAuth.currentUser;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      await _authService.register(name: name, email: email, password: password);
+
+      _user = _firebaseAuth.currentUser;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> logout() async {
+    await _authService.logout();
+    _user = null;
+    notifyListeners();
+  }
+}
