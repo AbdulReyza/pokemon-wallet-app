466639b6a659e377c2c2c3806a54571e9f381dbd
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-17 13:41:46 +0700

    bikin home wallet

--- lib/screens/home/home_wallet_screen.dart
+++ lib/screens/home/home_wallet_screen.dart
@@ -1,1 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
+class HomeWalletScreen extends StatelessWidget {
+  const HomeWalletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F7FA),
+
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xFFE3350D),
+        title: const Text(
+          "Pokémon Wallet",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+
+
+           
+          );
+        },
+      ),
+    );
+  }
+}
