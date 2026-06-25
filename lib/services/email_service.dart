01e021c1bebed7327d9062c1c8f7eee283d6a057
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-26 00:50:35 +0700

    send otp to gmail

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -3,15 +3,25 @@ import 'package:http/http.dart' as http;
 
 class EmailService {
   Future<void> sendOtp({required String email, required String otp}) async {
-    await http.post(
+    final response = await http.post(
       Uri.parse('https://api.emailjs.com/api/v1.0/email/send'),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({
         'service_id': 'service_55vqect',
         'template_id': 'template_x8fc706',
         'user_id': 'wC-lCh6sb90jUwDCT',
+        'accessToken': 'IIXa7xxJ66Ggw3IzR8OW5',
         'template_params': {'to_email': email, 'otp': otp},
       }),
     );
+
+    print("========== EMAILJS ==========");
+    print("STATUS : ${response.statusCode}");
+    print("BODY   : ${response.body}");
+    print("=============================");
+
+    if (response.statusCode != 200) {
+      throw Exception("Gagal mengirim OTP. Status: ${response.statusCode}");
+    }
   }
 }
